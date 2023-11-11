50097d0e2c47f305e9a95ade1ba18eb35c403089
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-11-11 03:54:05 +0200

    Added main code

---
+++ main.py
@@ -0,0 +1,13 @@
+from numberConverter.numberConverter import convert_to_base_n, convert_base
+
+
+
+
+# Example usage:
+number_a = "1A"  # Replace with your number
+number_b = "1B"
+
+result = add_numbers(number_a, number_b, 16)
+#result = convert_base(number_to_convert, from_base, to_base)
+print(f"HEllo: {number_a} in base {number_b} is {result} in base 16")
+
