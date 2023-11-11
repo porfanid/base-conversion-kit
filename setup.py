43ff661a151ff4d53aa505ace5fbb101c4e10677
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2023-11-11 05:04:59 +0200

    Added funding links

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='base-conversion-kit',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    project_urls={
+        'Funding': 'https://ko-fi.com/porfanid',
+    }
 )
